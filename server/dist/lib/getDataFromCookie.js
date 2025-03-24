a8557edb31567f3e3d3d3aa2b8b7987b6bf8077e
Harshiile~ <theharshiile@gmail.com~>
2025-03-24 22:15:46 +0530

    Containerize Done

--- server/dist/lib/getDataFromCookie.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getDataFromCookie = void 0;
-const cookie_1 = require("./cookie");
-const getDataFromCookie = (cookieName) => {
-    return (0, cookie_1.cookieValidator)(cookieName);
-};
-exports.getDataFromCookie = getDataFromCookie;
